a52ae8120b58d222a97882c2ba6a0917ee08dda4
Siesqo <jeroen@siesqo.be>
2013-10-03 08:16:10 +0200

    Distance::getClosest() added
    This functions calculates the distances between your defined location
    and the other locations and it will return the closest location + the
    distance to it.

--- src/Distance/Distance.php
+++ src/Distance/Distance.php
@@ -16,9 +16,11 @@ class Distance
 	 * @param decimal $longitude1
 	 * @param decimal $latitude2
 	 * @param decimal $longitude2
+	 * @param int $decimals[optional] The amount of decimals
+	 * @param string $unit[optional]
 	 * @return float
 	 */
-	public static function between($latitude1, $longitude1, $latitude2, $longitude2, $unit = 'km')
+	public static function between($latitude1, $longitude1, $latitude2, $longitude2, $decimals = 1, $unit = 'km')
 	{
 		$theta = $longitude1 - $longitude2;
 		$distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2))) + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta)));
@@ -33,7 +35,41 @@ class Distance
 		}
 
 		// return with one decimal
-		return (round($distance, 1));
+		return round($distance, $decimals);
+	}
+
+	/**
+	 * Get closest location from all locations
+	 *
+	 * @param decimal $latitude1
+	 * @param decimal $longitude1
+	 * @param int $decimals[optional] The amount of decimals
+	 * @return array The item which is the closest
+	 */
+	public static function getClosest($latitude1, $longitude1, $items, $decimals = 1)
+	{
+		// init result
+		$distances = array();
+
+		// loop items
+		foreach($items as $key => $item)
+		{
+			// define second item
+			$latitude2 = $item['latitude'];
+			$longitude2 = $item['longitude'];
+
+			// define distance
+			$distance = self::between($latitude1, $longitude1, $latitude2, $longitude2, 10);
+
+			// add distance
+			$distances[$distance] = $key;
+
+			// add rounded distance to array
+			$items[$key]['distance'] = round($distance, $decimals);
+		}
+
+		// return the item with the closest distance
+		return $items[$distances[min(array_keys($distances))]];
 	}
 }
 
--- tests/index.php
+++ tests/index.php
@@ -8,18 +8,38 @@
  * @date 20130826
  */
  
- // require
- require '../src/Distance/Distance.php';
- 
- // demo variables
- $latitude1 = '';
- $longitude1 = '';
- $latitude2 = '';
- $longitude2 = '';
- 
- // get distance between
- $distance = Distance::between($latitude1, $longitude1, $latitude2, $longitude2);
- 
- // dump data
- echo 'Distance between is ' . $distance . ' km';
+// require
+require '../src/Distance/Distance.php';
+
+// demo variables
+$latitude1 = '50.8538510000';
+$longitude1 = '3.3550450000';
+$latitude2 = '50.8325600000';
+$longitude2 = '3.4787650000';
+
+// define multiple items
+$items = array(
+	array(
+		'title' => 'waregem',
+		'latitude' =>'50.8865040000',
+		'longitude' => '3.4320850000'
+	),
+	array(
+		'title' => 'Anzegem',
+		'latitude' =>'50.8325600000',
+		'longitude' => '3.4787650000'
+	)
+);
+
+// get distance between
+$distance = Distance::between($latitude1, $longitude1, $latitude2, $longitude2);
+
+// dump data
+echo 'Distance between is ' . $distance . ' km';
+
+// get closest distance between
+$distance = Distance::getClosest($latitude1, $longitude1, $items);
+
+// dump data
+echo 'Distance between is ' . $distance['distance'] . ' km';
  
