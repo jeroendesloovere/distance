649f46379e598a0ef006609bb4506d835ea9373b
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-03-19 09:53:55 +0100

    PSR-2-code-styling applied

--- src/Distance/Distance.php
+++ src/Distance/Distance.php
@@ -9,68 +9,68 @@
  */
 class Distance
 {
-	/**
-	 * Get distance between two coordinates
-	 *
-	 * @param decimal $latitude1
-	 * @param decimal $longitude1
-	 * @param decimal $latitude2
-	 * @param decimal $longitude2
-	 * @param int $decimals[optional] The amount of decimals
-	 * @param string $unit[optional]
-	 * @return float
-	 */
-	public static function between($latitude1, $longitude1, $latitude2, $longitude2, $decimals = 1, $unit = 'km')
-	{
-		$theta = $longitude1 - $longitude2;
-		$distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2))) + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta)));
-		$distance = acos($distance);
-		$distance = rad2deg($distance);
-		$distance = $distance * 60 * 1.1515;
+    /**
+     * Get distance between two coordinates
+     *
+     * @return float
+     * @param decimal $latitude1
+     * @param decimal $longitude1
+     * @param decimal $latitude2
+     * @param decimal $longitude2
+     * @param int $decimals[optional] The amount of decimals
+     * @param string $unit[optional]
+     */
+    public static function between($latitude1, $longitude1, $latitude2, $longitude2, $decimals = 1, $unit = 'km')
+    {
+        // define calculation variables
+        $theta = $longitude1 - $longitude2;
+        $distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2))) + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta)));
+        $distance = acos($distance);
+        $distance = rad2deg($distance);
+        $distance = $distance * 60 * 1.1515;
 
-		// unit
-		if($unit == 'km')
-		{
-			$distance = $distance * 1.609344;
-		}
+        // unit is km
+        if ($unit == 'km') {
+            // redefine distance
+            $distance = $distance * 1.609344;
+        }
 
-		// return with one decimal
-		return round($distance, $decimals);
-	}
+        // return with one decimal
+        return round($distance, $decimals);
+    }
 
-	/**
-	 * Get closest location from all locations
-	 *
-	 * @param decimal $latitude1
-	 * @param decimal $longitude1
-	 * @param int $decimals[optional] The amount of decimals
-	 * @return array The item which is the closest + 'distance' to it.
-	 */
-	public static function getClosest($latitude1, $longitude1, $items, $decimals = 1)
-	{
-		// init result
-		$distances = array();
+    /**
+     * Get closest location from all locations
+     *
+     * @return array The item which is the closest + 'distance' to it.
+     * @param decimal $latitude1
+     * @param decimal $longitude1
+     * @param int $decimals[optional] The amount of decimals
+     */
+    public static function getClosest($latitude1, $longitude1, $items, $decimals = 1)
+    {
+        // init result
+        $distances = array();
 
-		// loop items
-		foreach($items as $key => $item)
-		{
-			// define second item
-			$latitude2 = $item['latitude'];
-			$longitude2 = $item['longitude'];
+        // loop items
+        foreach ($items as $key => $item) {
+            // define second item
+            $latitude2 = $item['latitude'];
+            $longitude2 = $item['longitude'];
 
-			// define distance
-			$distance = self::between($latitude1, $longitude1, $latitude2, $longitude2, 10);
+            // define distance
+            $distance = self::between($latitude1, $longitude1, $latitude2, $longitude2, 10);
 
-			// add distance
-			$distances[$distance] = $key;
+            // add distance
+            $distances[$distance] = $key;
 
-			// add rounded distance to array
-			$items[$key]['distance'] = round($distance, $decimals);
-		}
+            // add rounded distance to array
+            $items[$key]['distance'] = round($distance, $decimals);
+        }
 
-		// return the item with the closest distance
-		return $items[$distances[min(array_keys($distances))]];
-	}
+        // return the item with the closest distance
+        return $items[$distances[min(array_keys($distances))]];
+    }
 }
 
 
