36c1d3cb6dd74090a5dc5a6d7a6e5483499bd051
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-04-02 11:38:03 +0200

    Exception added again

--- src/JeroenDesloovere/Distance/Distance.php
+++ src/JeroenDesloovere/Distance/Distance.php
@@ -74,3 +74,11 @@ class Distance
         return $items[$distances[min(array_keys($distances))]];
     }
 }
+
+
+/**
+ * Distance Exception
+ *
+ * @author Jeroen Desloovere <info@jeroendesloovere.be>
+ */
+class DistanceException extends \Exception {}
